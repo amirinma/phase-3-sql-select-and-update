0d7e32358752cb16be9ed13f0738d66d87e14922
Mohammad <zia.ro@live.com>
2022-08-05 00:05:53 -0400

    Completed Assingment

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 5, "American Shorthair");
+INSERT INTO cats (name, age, breed) VALUES ("Hannah", 1, "Tabby");
+INSERT INTO cats (name, age, breed) VALUES ("Khoshbo", 4, "Ate Patato");
+UPDATE cats SET name = "Khoshbo Gak" WHERE name = "Khoshbo";
+DELETE FROM cats WHERE id = 4;
